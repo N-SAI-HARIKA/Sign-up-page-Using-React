bf8315f1bc217bc3925f3d901af09697eb8677e7
Sai Harika Nanduri <118583056+N-SAI-HARIKA@users.noreply.github.com>
2024-10-22 10:19:19 +0530

    Add files via upload

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import '../App.css';
+const Dashboard = ({ setIsLoggedIn }) => {
+  const navigate = useNavigate();
+  function handleLogout() {
+    setIsLoggedIn(false);
+    localStorage.removeItem("isLoggedIn");
+    navigate("/login");
+  }
+  return (
+    <div className="dashboard-container">
+      <h2>Welcome to your Dashboard</h2>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
 
+export default Dashboard;
